838495a713c59f74e5f357c61140ee145ae48e07
chaewon <cwlee4633@naver.com>
2024-02-14 16:25:25 +0900

    회원 기능 테스트

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -20,6 +20,7 @@ public class MemberService {
      */
     @Transactional
     public Long join(Member member) {
+
         validateDuplicateMember(member); //중복 회원 검증
         memberRepository.save(member);
         return member.getId();
@@ -33,11 +34,12 @@ public class MemberService {
     }
 
     //회원 전체 조회
-
     public List<Member> findMembers() {
         return memberRepository.findAll();
     }
+
     public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
     }
+
 }
