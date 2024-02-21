aff4aaf0b74a328fe6f72f5b4abcfd1312631d6b
chaewon <cwlee4633@naver.com>
2024-02-21 19:39:54 +0900

    주문 조회 v4

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -14,9 +14,9 @@ public class JpashopApplication {
     @Bean
     Hibernate6Module hibernate6Module() {
         Hibernate6Module hibernate6Module = new Hibernate6Module();
-        //강제 지연 로딩 설정
-        hibernate6Module.configure(Hibernate6Module.Feature.FORCE_LAZY_LOADING,
-                true);
+//        //강제 지연 로딩 설정
+//        hibernate6Module.configure(Hibernate6Module.Feature.FORCE_LAZY_LOADING,
+//                true);
         return hibernate6Module;
     }
 }
